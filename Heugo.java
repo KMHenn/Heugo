82f0802e016648416115eecc5eb18c326765bd80
Kaitlyn <kaithennessy@gmail.com>
2017-07-07 13:25:38 +0900

    Initial commit

---
+++ Heugo.java
@@ -0,0 +1,22 @@
+package Heugo;
+
+import java.util.List;
+
+import negotiator.actions.Action;
+import negotiator.parties.AbstractNegotiationParty;
+
+public class Heugo extends AbstractNegotiationParty {
+
+	@Override
+	public Action chooseAction(List<Class<? extends Action>> arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
